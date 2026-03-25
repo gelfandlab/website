bc5e9a6f0942470dd71e5d3d6c6a98a8ff3397ad
FionaN <fiona1941nicdao@gmail.com>
2026-03-25 15:45:42 -0500

    start background image

--- website/src/pages/Home.jsx
+++ website/src/pages/Home.jsx
@@ -1,9 +1,17 @@
+import { useState, useEffect } from "react"
+import "../assets/backgroundimage.css";
+import "../assets/images/Terminal_Cell_FITC.jpeg"
+
+const image = [
+// need to finish
+]
+
 export default function Home() {
   return (
     <>
         <section id="home" className="section">
             <h1> Gelfand Lab!</h1>
-            <h2> at Northwestern University, Chicago IL</h2>
+            <h2> Northwestern University, Chicago IL</h2>
             <p>
                 We study the role of the cytoskeleton in diverse biological processes, 
                 including neurite outgrowth, polarity determination, and intracellular dynamics.
